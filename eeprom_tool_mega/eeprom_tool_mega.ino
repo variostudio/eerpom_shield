d207c8e81dbaa7769a118c1ed4d65642d04df78f
Vario Studio <variostudio@users.noreply.github.com>
2022-11-24 19:37:24 -0500

    Full address range support

--- eeprom_tool_mega/eeprom_tool_mega.ino
+++ eeprom_tool_mega/eeprom_tool_mega.ino
@@ -1,5 +1,5 @@
 #define data_pin_cnt 8
-#define addr_pin_cnt 16
+#define addr_pin_cnt 19 //Address pins number: SST39SF040 - 19, SST39SF020A - 18, SST39SF010A - 17
 
 #define ce_pin 23 //CE# - chip enable, active low
 #define oe_pin 21 //OE# - output enable, active low
@@ -8,11 +8,11 @@
 #define read_led 41
 
 int currentAddress = 0;
-int addr_pins[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+int addr_pins[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
 int data_pins[] = {24,25,26,27,28,29,30,31};
 
 
-byte data[] = {0x1a, 0xa2, 0x15, 0xce, 0x20};
+byte data[] = {0x1a, 0xa2, 0x15, 0xce, 0x20, 0xbb, 0xcc, 0x97};
 
 void toAddressPins(int value) {
   for (int i = 0, mask = 1; i < addr_pin_cnt; i++, mask = mask << 1) {
@@ -115,7 +115,6 @@ void getChipId() {
   Serial.print("ID: ");
   Serial.println(res, HEX);
 
-
   toAddressPins(1);
 
   res = readByte();
@@ -217,13 +216,14 @@ void setup() {
   initPins();
 
   Serial.begin(9600); 
+  Serial.println("Welcome to Arduino EEPROM SST39SF0xx programmer!");
 
   dataOut();
   chipErase();
 
   delay(1000);
 
-  for (int idx=0; idx<5; idx++) {
+  for (int idx=0; idx<sizeof(data)/sizeof(data[0]); idx++) {
     programByte(idx, data[idx]);
   }
   
